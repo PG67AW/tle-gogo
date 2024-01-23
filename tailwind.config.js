bfd2ead92256c62a789436fe1417acacade68b5c
saksri-eqho <saksri.s@eqho.com>
2024-01-23 13:06:30 +0700

    Add bean and new font to genensis

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ["'Chivo'", ...defaultTheme.fontFamily.sans],
             },
         },
     },
