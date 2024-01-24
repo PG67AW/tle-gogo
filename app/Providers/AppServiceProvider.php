5a12fc2ff84ef2ce203c92e11bbd537aa898e1cb
saksri-eqho <saksri.s@eqho.com>
2024-01-24 15:29:23 +0700

    Test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Visitor;
 use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
+use GuzzleHttp\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,7 +22,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Request $request): void
     {
-        $clientIpAddress = $request->getClientIp();
-        Visitor::create(['ip_address' => $clientIpAddress]);
+        $this->getUserInfo($request);
+    }
+
+    public function getUserInfo($request)
+    {
+        // Get the user's IP address
+        $userIp = $request->ip();
+        // Make a request to the ipinfo.io API
+        $client = new Client();
+        $response = $client->get("https://ipinfo.io/{$userIp}?token=7e5a5662ef114c");
+        // Parse the JSON response
+        $data = json_decode($response->getBody());
+        // Extract user information
+        $location = $data->loc;
+        $country = $data->country;
+        $currency = $data->currency;
+        dd($data);
+        // You can return the user information or use it as needed
+        return view('user-info', compact('location', 'country', 'currency'));
     }
 }
